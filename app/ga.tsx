1e6d88b5333e30b9405268902b739f52b52b23e3
Martín Triay <martriay@gmail.com>
2023-06-04 23:32:36 -0300

    add ga.tsx

---
+++ app/ga.tsx
@@ -0,0 +1,18 @@
+import Script from 'next/script'
+
+export default function Analytics() {
+    return <>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        
+        <Script strategy="lazyOnload">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+        </Script>
+    </>
+}
